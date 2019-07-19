180b3ae1762dab2ecbf474068f4d4cad97fd7b88
tapas100 <tapas.refixd>
2019-07-19 13:09:43 +0530

    bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoaderService } from './services/loader.service';
 import { LoaderComponent } from './components/loader/loader.component';
 import { HomeModule } from './home/home.module';
 import { RouterModule } from '@angular/router';
+import { HttpService } from './services/http.service';
+import { HttpcacheService } from './services/httpcache.service';
+import { CacheInterceptor } from './interceptors/cache.interceptor';
 import { MaterialModule } from './material.module';
-
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +27,18 @@ import { MaterialModule } from './material.module';
     HttpClientModule,
     BrowserAnimationsModule,
     RouterModule.forRoot([])
+  ],
+  providers: [LoaderService, HttpService, HttpcacheService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CacheInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
