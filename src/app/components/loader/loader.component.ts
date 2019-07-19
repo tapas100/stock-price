543b69564d1798b5221034f449815a3cb0c01e57
tapas100 <tapas.refixd>
2019-07-19 12:57:31 +0530

    loader Added

---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getArray(n){
+    return new Array(n)
+  }
+
+}
